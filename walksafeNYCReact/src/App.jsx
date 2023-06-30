4e637b68b5ef2c6be3f7b4a2bf76cdc05d59e794
Margaret Camilletti <mhcamilletti@gmail.com>
2023-06-30 11:03:18 -0400

    updated App.jsx

--- walksafeNYCReact/src/App.jsx
+++ walksafeNYCReact/src/App.jsx
@@ -4,7 +4,15 @@ import Main from './components/Main'
 import Footer from './components/Footer'
 
 function App() {
-
+  return (
+    <div className="App">
+      <main className="app-main">
+        <Main />
+      </main>
+      <footer className="app-footer">
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
