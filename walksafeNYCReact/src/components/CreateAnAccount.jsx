484b3db1acdbb0b6d22a48e09703081bac186867
Margaret Camilletti <mhcamilletti@gmail.com>
2023-06-30 14:23:59 -0400

    created forms for create an account and login...they are ugly but they work

--- walksafeNYCReact/src/components/CreateAnAccount.jsx
+++ walksafeNYCReact/src/components/CreateAnAccount.jsx
@@ -1,6 +1,41 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+
 const CreateAnAccount = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [dateOfBirth, setDateOfBirth] = useState('')
+    const [gender, setGender] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email)
+    }
+
+
     return (
-        <h4>This is the create an account page.</h4>
+        <div className="create-account-page">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input onChange={(e)=>setFirstName(e.target.value)} type="text" placeholder="First Name" value={firstName} name="firstName" id="firstName"/>
+                <label htmlFor="lastName">Last Name</label>
+                <input onChange={(e)=>setLastName(e.target.value)}type="text" placeholder="Last Name" value={lastName} name="lastName" id="lastName"/>
+                <label htmlFor="dateOfBirth">Date of Birth</label>
+                <input onChange={(e)=>setDateOfBirth(e.target.value)}type="date" placeholder="Date of Birth" value={dateOfBirth} name="dateOfBirth" id="dateOfBirth"/>
+                <label htmlFor="gender">Gender</label>
+                <input onChange={(e)=>setGender(e.target.value)}type="text" placeholder="gender" value={gender} name="gender" id="gender"/>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" type="email" placeholder="youremail@gmail.com"/>
+                <label htmlFor="password">password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password" placeholder="*******"/>
+                <button type="submit">Create Account</button>
+            </form>
+            <Link class="login" to="/login"><button>Already have an account? Log in here.</button></Link>
+        </div>
     )
 }
 
--- walksafeNYCReact/src/components/Login.jsx
+++ walksafeNYCReact/src/components/Login.jsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom'
-import Main from './Main'
+import React, { useState } from 'react'
 
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email)
+    }
+
     return (
         <div className="login-page">
-            <form>
-                <label for="email">email</label>
-                <input name="email" id="email" type="email" placeholder="youremail@gmail.com"/>
-                <label for="password">password</label>
-                <input name="password" id="password" type="password" placeholder="*******"/>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" type="email" placeholder="youremail@gmail.com"/>
+                <label htmlFor="password">password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password" placeholder="*******"/>
+                <button type="submit">Log in</button>
             </form>
-            <Link class="createAccount" to="/createAnAccount">Create An Account</Link>
+            <Link class="createAccount" to="/createAnAccount"><button>Create An Account</button></Link>
         </div>
     )
 }
