0380697f5a1b08d61e3b8347b60a404728292e4d
Vincent Vullo <vincevullo@gmail.com>
2023-06-30 12:13:55 -0400

    added first iteration of map

--- walksafeNYCReact/src/components/Home.jsx
+++ walksafeNYCReact/src/components/Home.jsx
@@ -1,8 +1,11 @@
+import Map from "./Map"
 const Home = () => {
     return  (
         <div className="homePage">
             <h3>Walk Safe NYC</h3>
             <h4>Report street harassment and help keep other women safe.</h4>
+            <Map />
+            
         </div>
     )
 }
