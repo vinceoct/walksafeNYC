5f5ee78c281fdbc91cd037b9edd81f6738c063a5
Vincent Vullo <vincevullo@gmail.com>
2023-07-02 05:37:45 -0400

    moved name from main to header

--- walksafeNYCReact/src/components/Header.jsx
+++ walksafeNYCReact/src/components/Header.jsx
@@ -2,7 +2,10 @@ import Nav from './Nav'
 
 const Header = () => {
     return (
+        <div className='header'>
+        <h3>Walk Safe NYC</h3>
         <Nav />
+        </div>
     )
 }
 
--- walksafeNYCReact/src/components/Home.jsx
+++ walksafeNYCReact/src/components/Home.jsx
@@ -2,7 +2,6 @@ import Map from "./Map"
 const Home = () => {
     return  (
         <div className="homePage">
-            <h3>Walk Safe NYC</h3>
             <h4>Report street harassment and help keep other women safe.</h4>
             <Map />
             
