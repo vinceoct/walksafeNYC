3d661b19787ca70748de37d8f2db49d3dfcf19d8
Margaret Camilletti <mhcamilletti@gmail.com>
2023-07-02 00:28:06 -0400

    styled forms for login, create an account, and submit a report and set up pages for user profile page and individual report page

--- walksafeNYCReact/src/components/CreateAnAccount.jsx
+++ walksafeNYCReact/src/components/CreateAnAccount.jsx
@@ -19,22 +19,35 @@ const CreateAnAccount = () => {
 
     return (
         <div className="create-account-page">
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="firstName">First Name</label>
-                <input onChange={(e)=>setFirstName(e.target.value)} type="text" placeholder="First Name" value={firstName} name="firstName" id="firstName"/>
-                <label htmlFor="lastName">Last Name</label>
-                <input onChange={(e)=>setLastName(e.target.value)}type="text" placeholder="Last Name" value={lastName} name="lastName" id="lastName"/>
-                <label htmlFor="dateOfBirth">Date of Birth</label>
-                <input onChange={(e)=>setDateOfBirth(e.target.value)}type="date" placeholder="Date of Birth" value={dateOfBirth} name="dateOfBirth" id="dateOfBirth"/>
-                <label htmlFor="gender">Gender</label>
-                <input onChange={(e)=>setGender(e.target.value)}type="text" placeholder="gender" value={gender} name="gender" id="gender"/>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" type="email" placeholder="youremail@gmail.com"/>
-                <label htmlFor="password">password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password" placeholder="*******"/>
-                <button type="submit">Create Account</button>
+            <h4>Create an account</h4>
+            <form className="create-account-form" onSubmit={handleSubmit}>
+                <div className="account-question">
+                    <label htmlFor="firstName">First Name</label>
+                    <input onChange={(e)=>setFirstName(e.target.value)} type="text" placeholder="First Name" value={firstName} name="firstName" id="firstName"/>
+                </div>
+                <div className="account-question">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input onChange={(e)=>setLastName(e.target.value)}type="text" placeholder="Last Name" value={lastName} name="lastName" id="lastName"/>
+                </div>
+                <div className="account-question">
+                    <label htmlFor="dateOfBirth">Date of Birth</label>
+                    <input onChange={(e)=>setDateOfBirth(e.target.value)}type="date" placeholder="Date of Birth" value={dateOfBirth} name="dateOfBirth" id="dateOfBirth"/>
+                </div>
+                <div className="account-question">
+                    <label htmlFor="gender">Gender</label>
+                    <input onChange={(e)=>setGender(e.target.value)}type="text" placeholder="gender" value={gender} name="gender" id="gender"/>
+                </div>
+                <div className="account-question">
+                    <label htmlFor="email">email</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" type="email" placeholder="youremail@gmail.com"/>
+                </div>
+                <div className="account-question">
+                    <label htmlFor="password">password</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password" placeholder="*******"/>
+                </div>
+                <button id="create-account-button" className="submit-button" type="submit">Create Account</button>
+                <Link class="login" to="/login"><button id="already-have-account" className="submit-button">Already have an account? Log in here.</button></Link>
             </form>
-            <Link class="login" to="/login"><button>Already have an account? Log in here.</button></Link>
         </div>
     )
 }
---
+++ walksafeNYCReact/src/components/UserPage.jsx
@@ -0,0 +1,9 @@
+const UserPage = () => {
+    return (
+        <div className="userPage">
+            <h4>Profile Page</h4>
+        </div>
+    )
+}
+
+export default UserPage
