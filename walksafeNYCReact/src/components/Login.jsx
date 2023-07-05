f36aa736f6139a6e0e84a810036f73556458a7f5
LowellC86 <lowellcaraballo@gmail.com>
2023-07-05 18:33:59 -0400

    login info valid credentials

--- walksafeNYCReact/src/components/Login.jsx
+++ walksafeNYCReact/src/components/Login.jsx
@@ -1,37 +1,89 @@
-import { Link } from 'react-router-dom'
-import React, { useState } from 'react'
-import city from '../assets/city.png'
+import React, { useState, useEffect } from 'react';
+import city from '../assets/city.png';
+import axios from 'axios';
 
+const initialState = {
+  email: '',
+  password: '',
+  passwordConfirm: '',
+  valid: '',
+};
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [formState, setFormState] = useState(initialState);
+  const [users, setUsers] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(email)
+  useEffect(() => {
+    fetchOneUser();
+  }, []);
+
+  const fetchOneUser = async () => {
+    try {
+      const response = await axios.get("https://walksafenyc-api-production.up.railway.app/api/users");
+      setUsers(response.data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
     }
+  };
 
-    return (
-        <div className="login-page">
-            <div className='form-container'>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <h4>Login to your account.</h4>
-                <div className="login-question">
-                    <label htmlFor="email">Email:</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" type="email" placeholder="youremail@gmail.com"/>
-                </div>
-                <div className="login-question">
-                    <label htmlFor="password">Password:</label>
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password" placeholder="*******"/>
-                </div>
-                <button id="log-in-button" className="submit-button" type="submit">Log in</button>
-                <Link class="createAccount" to="/createAnAccount"><button id="create-account" className="submit-button">Create An Account</button></Link>
-            </form>
-            <img src={city}/>
-            </div>
-        </div>
-    )
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormState(initialState);
 
-export default Login
+    const { email, password } = formState;
+
+    const selectedUser = users.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (selectedUser) {
+      console.log('Logged in:', selectedUser);
+      setFormState({ ...formState, valid: 'Credentials are correct' });
+    } else {
+      setFormState({ ...formState, valid: 'Invalid credentials' });
+    }
+  };
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="login-page">
+      <div className="form-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <h4>Login to your account.</h4>
+          <div className="login-question">
+            <label htmlFor="email">Email:</label>
+            <input
+              value={formState.email}
+              onChange={handleChange}
+              name="email"
+              id="email"
+              type="email"
+              placeholder="youremail@gmail.com"
+            />
+          </div>
+          <div className="login-question">
+            <label htmlFor="password">Password:</label>
+            <input
+              value={formState.password}
+              onChange={handleChange}
+              name="password"
+              id="password"
+              type="password"
+              placeholder="*******"
+           />
+          </div>
+          <button id="log-in-button" className="submit-button" type="submit">
+            Log in
+          </button>
+          {formState.valid && <p>{formState.valid}</p>}
+        </form>
+        <img src={city} alt="City" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
