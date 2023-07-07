12ffc992e728bd0ea3454a12cf30255c8c548465
Margaret Camilletti <mhcamilletti@gmail.com>
2023-07-07 15:24:00 -0400

    updated nav to show when a user is logged in

--- walksafeNYCReact/src/components/Nav.jsx
+++ walksafeNYCReact/src/components/Nav.jsx
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/AuthProvider'
+import React, { useContext } from 'react';
+
 
 const Nav = () => {
+    const { loggedIn, user } = useContext(AuthContext)
+
+    if (!loggedIn) {
     return (
         <div className="nav-links">
             <Link className="nav" to="/">Home</Link>
@@ -9,6 +15,16 @@ const Nav = () => {
             <Link className="nav" to="/reports">View all reports</Link>
         </div>
     )
+    } else {
+        return (
+            <div className="nav-links">
+                <Link className="nav" to="/">Home</Link>
+                <Link className="nav" to="/reportForm">Submit a Report</Link>
+                <Link className="nav" to="/login/:id">Account</Link>
+                <Link className="nav" to="/reports">View all reports</Link>
+            </div>
+        )
+    }
 }
 
 export default Nav
